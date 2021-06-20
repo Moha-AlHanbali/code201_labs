7ff6aa8d95d70b9d653cffba519f4600b3c58bc8
Moha-AlHanbali <Moha.AlHanbali@gmail.com>
2021-06-20 15:18:53 +0300

    Created lab01b

---
+++ js/app.js
@@ -0,0 +1,33 @@
+alert("Hello! Welcome to this Webpage!");
+
+let visitorName= prompt("What should I call you?");
+
+alert("I hope you're doing well, " + visitorName + ".");
+
+console.log("Name: ", visitorName, typeof visitorName);
+
+let genre= prompt("What is your favorite video games genre " + visitorName + "?");
+
+alert("Oh! " + genre + ", good taste!");
+
+console.log("Favorite genre: ", genre, typeof genre);
+
+let game= prompt("What is your favorite video game among " + genre + " games?");
+
+alert(game + "! That is a fun one!");
+
+console.log("Game title: ", game, typeof game);
+
+let troll= prompt("How many hours do you play it a week?");
+
+console.log("Playing frequency: ", troll, typeof troll);
+
+
+alert("Uh... " + troll + ".. I think you may need to find a more balanced life style, " + visitorName + "...");
+
+alert("Well, thank you for taking time " + visitorName + ", I hope you enjoy playing " + game +".");
+
+alert("May you find more fun " + genre + " games! And do something about this " + troll + " hours a week problem..");
+
+
+
